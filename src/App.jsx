901ede86417544bd78c2b0da788f882efa77e72b
Ruderson Florentino <rudersonvc@gmail.com>
2024-09-09 22:17:48 -0300

    feat: create routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,14 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Client from "./pages/Client";
+
 function App() {
-  return <h1>Portfolio</h1>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route index path="/" element={<Client />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
