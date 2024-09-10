f0c5cd6ecae0c0252a5bc071b82ad156e5669bfe
Ruderson Florentino <rudersonvc@gmail.com>
2024-09-09 22:14:47 -0300

    feat: create client page

---
+++ src/pages/Client/index.jsx
@@ -0,0 +1,13 @@
+import styles from "./styles.module.scss";
+
+function Client() {
+  return (
+    <>
+      <div>Header</div>
+      <div>Client</div>
+      <div>Footer</div>
+    </>
+  );
+}
+
+export default Client;
