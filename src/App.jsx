5058809d58174e858cee88790c5d7066fec18b5c
Ruderson Florentino <rudersonvf@gmail.com>
2024-09-27 11:15:11 -0300

    feat: add admin category page, not finalized

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,16 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Client from "./pages/Client";
 import "./sass/app.scss";
 import Admin from "./pages/Admin";
+import Categories from "./pages/Admin/Categories";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route index path="/" element={<Client />} />
-        <Route path="/admin" element={<Admin />} />
+        <Route path="/admin/" element={<Admin />}>
+          <Route path="categories" element={<Categories />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
---
+++ src/pages/Admin/Categories/index.jsx
@@ -0,0 +1,18 @@
+const Categories = () => {
+  return (
+    <main>
+      <section>
+        <div className="container">
+          <div className="content">
+            <h3>Categorias</h3>
+            <div className="collapse" id="collapseCat">
+              <p>formulario para as categorias</p>
+            </div>
+          </div>
+        </div>
+      </section>
+    </main>
+  );
+};
+
+export default Categories;
