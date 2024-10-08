a3fa516ebc6f9787d8634415156919b09f41518d
Ruderson Florentino <rudersonvf@gmail.com>
2024-10-08 11:16:51 -0300

    feat: dropdown menu on admin area

--- src/components/AdminHeader/index.jsx
+++ src/components/AdminHeader/index.jsx
@@ -1,9 +1,17 @@
+import { useState } from "react";
 import PropTypes from "prop-types";
-import { FaBell } from "react-icons/fa6";
+import { NavLink } from "react-router-dom";
+import { FaBell, FaNewspaper, FaPowerOff } from "react-icons/fa6";
 import imgOwner from "../../assets/ruderson.webp";
 import styles from "./styles.module.scss";
 
 const AdminHeader = ({ onToggleAside }) => {
+  const [isActive, setIsActive] = useState(false);
+
+  const handleDropdownClick = () => {
+    setIsActive(!isActive);
+  };
+
   return (
     <header className={styles["component-admin-header"]}>
       <div className="container d-flex justify-content-between align-items-center">
@@ -14,9 +22,28 @@ const AdminHeader = ({ onToggleAside }) => {
         </div>
         <nav>
           <FaBell />
-          <div className={styles["img-container"]}>
+          <div
+            className={styles["img-container"]}
+            onClick={handleDropdownClick}
+          >
             <img src={imgOwner} alt="Ruderson" />
           </div>
+          {isActive && (
+            <div className={styles["dropdown-container"]}>
+              <ul>
+                <li>
+                  <NavLink to="/">
+                    <FaNewspaper />
+                    To site
+                  </NavLink>
+                </li>
+                <li>
+                  <FaPowerOff />
+                  Logout
+                </li>
+              </ul>
+            </div>
+          )}
         </nav>
       </div>
     </header>
