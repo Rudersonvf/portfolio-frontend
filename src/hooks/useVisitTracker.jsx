26f79661e4da4c6611f8fc2a3b6873247e6ce5ea
Ruderson Florentino <rudersonvc@gmail.com>
2024-10-27 20:23:20 -0300

    fix: adjust tracker hook

--- src/hooks/useVisitTracker.jsx
+++ src/hooks/useVisitTracker.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 
 import { useLocation } from "react-router-dom";
 
@@ -6,39 +6,49 @@ import { requestBackend } from "../lib/axios";
 
 function useVisitTracker() {
   const location = useLocation();
+  const previousPath = useRef(location.pathname);
+  const startTimeRef = useRef(Date.now());
 
   useEffect(() => {
-    const startTime = Date.now();
+    const endTime = Date.now();
+    const timeSpent = (endTime - startTimeRef.current) / 1000;
 
-    const sendVisitData = async () => {
-      const endTime = Date.now();
-      const timeSpent = (endTime - startTime) / 1000;
+    if (previousPath.current !== location.pathname) {
+      sendVisitData(timeSpent);
+      previousPath.current = location.pathname;
+      startTimeRef.current = Date.now();
+    }
 
-      try {
-        await requestBackend({
-          method: "POST",
-          url: "/api/visitors",
-          data: {
-            pageVisited: location.pathname,
-            timeSpent,
-            browser: navigator.userAgent,
-            device: /Mobi|Android/i.test(navigator.userAgent)
-              ? "Mobile"
-              : "Desktop",
-          },
-        });
-      } catch (error) {
-        console.error("Erro ao enviar dados de visita:", error);
-      }
+    const handleBeforeUnload = () => {
+      const totalTimeSpent = (Date.now() - startTimeRef.current) / 1000;
+      sendVisitData(totalTimeSpent);
     };
 
-    window.addEventListener("beforeunload", sendVisitData);
+    window.addEventListener("beforeunload", handleBeforeUnload);
 
     return () => {
-      window.removeEventListener("beforeunload", sendVisitData);
-      sendVisitData();
+      window.removeEventListener("beforeunload", handleBeforeUnload);
+      sendVisitData((Date.now() - startTimeRef.current) / 1000);
     };
   }, [location]);
-}
 
+  const sendVisitData = async (timeSpent) => {
+    try {
+      await requestBackend({
+        method: "POST",
+        url: "/api/visitors",
+        data: {
+          pageVisited: location.pathname,
+          timeSpent,
+          browser: navigator.userAgent,
+          device: /Mobi|Android/i.test(navigator.userAgent)
+            ? "Mobile"
+            : "Desktop",
+        },
+      });
+    } catch (error) {
+      console.error("Erro ao enviar dados de visita:", error);
+    }
+  };
+}
 export default useVisitTracker;
